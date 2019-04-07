0be88e90241564d13c486fa0f56e88a3111ec2ac
Frederik Hardervig <44330971+Fhardervig@users.noreply.github.com>
2019-04-07 16:14:02 -0700

    Create jsonloader.js

---
+++ jsonloader.js
@@ -0,0 +1,10 @@
+var requestURL = '/SpaceOpps.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'text';
+request.send();
+request.onload = function() {
+  var opportunitiestext = request.response;
+  var opportunities = JSON.parse(opportunitiestext);
+  console.log(opportunities);
+}
